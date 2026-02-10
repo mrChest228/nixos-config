d1b2d6cb0e635f384ff6e5fbf03115056e5ae411
mrChest228 <gengenm32111111@gmail.com>
2026-02-10 19:55:30 +0300

    Zsh functions

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -11,14 +11,20 @@
             path = "${config.xdg.dataHome}/zsh/zsh_history";
         };
 
-        shellAliases = {
+        siteFunctions = {
             update = ''
                 (
                     cd ${vars.configPath} && \
+                    nix flake update && \
                     git add . && \
-                    git commit -m "Update" && \
-                    nh os switch && \
-                    nh home switch && \
+                    if [ -z $1 ]; then
+                        git commit -m "$1"
+                    else
+                        echo "\e[1;32mGit commit name: \"Update $(date +\"%Y-%m-%d %H:%M\")\""
+                        git commit -m "Update $(date +\"%Y-%m-%d %H:%M\")"
+                    fi
+                    nh os switch . && \
+                    nh home switch . && \
                     nh clean all --keep 3 --keep-since 3d
                 )
             '';
@@ -26,8 +32,14 @@
                 (
                     cd ${vars.configPath} && \
                     git add . && \
-                    nh os switch && \
-                    nh home switch && \
+                    if [ -z $1 ]; then
+                        git commit -m "$1"
+                    else
+                        echo "\e[1;32mGit commit name: \"Rebuild $(date +\"%Y-%m-%d %H:%M\")\""
+                        git commit -m "Rebuild $(date +\"%Y-%m-%d %H:%M\")"
+                    fi
+                    nh os switch . && \
+                    nh home switch . && \
                     nh clean all --keep 3 --keep-since 3d
                 )
             '';
@@ -35,9 +47,17 @@
                 (
                     cd ${vars.configPath} && \
                     git add . && \
-                    nh home switch
+                    if [ -z $1 ]; then
+                        git commit -m "$1"
+                    else
+                        echo "\e[1;32mGit commit name: \"Reconf $(date +\"%Y-%m-%d %H:%M\")\""
+                        git commit -m "Reconf $(date +\"%Y-%m-%d %H:%M\")"
+                    fi
+                    nh home switch .
                 )
             '';
+        };
+        shellAliases = {
             tp = "trash-put";
         };
         
