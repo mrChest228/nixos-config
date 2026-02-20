a9bad10209a3dfeff2a584e0761b0d15405047a6
mrChest228 <gengenm32111111@gmail.com>
2026-02-20 13:02:35 +0300

    Reconf 2026-02-20 13:02

--- hosts/VICTUS/home-manager/home.nix
+++ hosts/VICTUS/home-manager/home.nix
@@ -7,7 +7,6 @@
         stateVersion = vars.systemVersion;
         enableNixpkgsReleaseCheck = false; # I use custom pkgs, they're mostly pkgs.unstable, like the unstable HM, but it doesn't know that 
         sessionVariables = {
-            FLAKE = vars.configPath;
         };
     };
 }
