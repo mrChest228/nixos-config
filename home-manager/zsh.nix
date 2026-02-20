7322eb684b7a45025f25597fd5987014e1c2b50f
mrChest228 <gengenm32111111@gmail.com>
2026-02-20 18:19:43 +0300

    Reconf 2026-02-20 18:19

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -1,5 +1,5 @@
 { config, pkgs, vars, ... }:
-{ 
+{
     programs.zsh = {
         enable = true;
         dotDir = "${config.xdg.configHome}/zsh";
@@ -28,6 +28,7 @@
                         # Print file changes
                         git status --porcelain | sed -e 's/^A/\x1b[32m+ /' -e 's/^D/\x1b[31m- /' -e 's/^M/\x1b[33mc /' -e 's/$/\x1b[0m/'
                         local msg="''${1:-Commit $(date +'%Y-%m-%d %H:%M')}"
+                        echo "$msg"
                         # Commit
                         silent git commit -m "$msg" && \
                         # Print commit info
