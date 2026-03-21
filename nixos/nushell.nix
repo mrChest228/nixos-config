3d69e3645e01eb0ad21c3134a5c23ca5ee6b04bc
mrChest228 <gengenm32111111@gmail.com>
2026-03-21 20:52:55 +0300

    Rebuild 2026-03-21 20:52

--- nixos/nushell.nix
+++ nixos/nushell.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, vars, ... }:
 {
-    programs.nushell.enable = true;
+    environment.shells = [ pkgs.unstable.nushell ];
     programs.bash.interactiveShellInit = '' # NuShell autostart
         if [[ "$TERM" != "dumb" ]] && [[ -z "$BASH_EXECUTION_STRING" ]]; then
             exec nu
