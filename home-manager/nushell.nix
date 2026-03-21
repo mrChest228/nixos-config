0f2d2c7becdcadef8939ec26e09f08e580c5dc03
mrChest228 <gengenm32111111@gmail.com>
2026-03-21 22:48:53 +0300

    Reconf 2026-03-21 22:48

---
+++ home-manager/nushell.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, vars, ... }:
+{
+    programs.nushell = {
+        enable = true;
+        settings = {
+            show_banner = false;
+        };
+        extraConfig = ''
+            def config-commit [message: string] {
+                cd ${vars.configPath} || return 1
+            }
+        '';
+        shellAliases = {
+            tp = "trash-put";
+        };
+    };
+}
