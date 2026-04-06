d1d9c9fd3bbc149e247550710d4fb33bffe7b4f2
mrChest228 <gengenm32111111@gmail.com>
2026-04-06 21:53:22 +0300

    Debug printes deleted

--- home-manager/nushell.nix
+++ home-manager/nushell.nix
@@ -84,8 +84,6 @@
                                 $pth = ($line | str substring 3.. | str replace (char nul) "")
                                 $out = if ($pth | str contains " ") { '"' + $pth + '"' } else { $pth }
                             }
-                            print $pth
-                            print ($pth | path exists)
                             mut time = "?"
                             if ($pth | path exists) {
                                 $time = (ls -D $pth | get 0 | get modified | format date '%Y-%m-%d %H:%M:%S')
