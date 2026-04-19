bc2ee846777550c126ac519ac8df7fab122df86b
mrChest228 <gengenm32111111@gmail.com>
2026-04-19 17:46:14 +0300

    Cachix

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -35,13 +35,33 @@ in
         channel.enable = false;
         settings = {
             http-connections = 20; # Number of parallel downloads
-            max-jobs = 14;          # Number of parallel compilations
+            max-jobs = 14;         # Number of parallel compilations
             # experimental-features = [ "nix-command" "flakes" ]; # Enabled by default in Determinate-nix
             use-xdg-base-directories = true;
             # Determinate
             auto-optimise-store = false; # Nh do it itself
             lazy-trees = true;
             eval-cores = 0;
+
+            substituters = [
+                "https://cache.nixos.org"
+                "https://nix-community.cachix.org"
+                # TODO: Leave one of them
+                "https://hyprland.cachix.org"
+                "https://niri.cachix.org"
+
+                "https://nix-gaming.cachix.org"
+            ];
+
+            trusted-public-keys = [
+                "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+                "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+
+                "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+                "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+
+                "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+            ];
         };
     };
     programs.nh = {
