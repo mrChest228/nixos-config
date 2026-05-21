8839d01b7867f92f3a834225016276966a667a28
mrChesr228 <gengenm32111111@gmail.com>
2026-05-21 16:15:52 +0300

    smart-boot-mount is done?

--- sys/smart-boot-mount.nix
+++ sys/smart-boot-mount.nix
@@ -1,24 +1,19 @@
 { config, lib, pkgs, vars, ... }: {
+    # boot.automount is based on LoaderDevicePartUUID that are given from bootloader. Systemd-boot gives this variable, but some unknown bootloader (maybe even GRUB2) doesn't give it. I hate this. This service mounts /boot only from UEFI variables
     boot.initrd.systemd = {
-        initrdBin = [
+        enable = true;
+        initrdBin = with pkgs; [
             coreutils
             util-linux
         ];
         services.smart-boot-mount = {
             description = "ESP mount based on UEFI BootCurrent/Boot000X variables only";
-            requires = [ # We need successful end of these utils to start us
-                "initrd-root-fs.target"          # After root (/sysroot) mount
-                "systemd-udev-settle.service"    # After the /dev/*
-                "sys-firmware-efi-efivars.mount" # After the efivars
-            ];
-            after = [
-                "initrd-root-fs.target"
-                "systemd-udev-settle.service"
-                "sys-firmware-efi-efivars.mount"
-            ];
+            # We need successful end of root (/sysroot) mount
+            requires = [ "initrd-root-fs.target" ];
+            after    = [ "initrd-root-fs.target" ];
 
             before   = [ "initrd-fs.target" ]; # Before all the root dirs mount target
-            requiredBy = [ "initrd-fs.target" ];
+            wantedBy = [ "initrd-fs.target" ];
 
             unitConfig.DefaultDependencies = false; # At initrd only
 
@@ -28,9 +23,10 @@
             };
 
             script = ''
-                # Fast exit if we are booting not in the UEFI-mode or /boot is already mounted
-                [ -d "/sysroot/boot" ] || exit -1
-                [ -d "/sys/firmware/efi/efivars" ] || exit 1
+                # Fast exit if we are booting not in the UEFI-mode or /boot is already mounted or I have no access to /dev/disk/by-partuuid
+                mkdir -p /sysroot/${config.boot.loader.efi.efiSysMountPoint}
+                [ "$(ls -A \"/sysroot/boot\" 2>/dev/null)" ] && exit -1
+                [ -d "/sys/firmware/efi/efivars" ] && [ "$(ls -A \"/sys/firmware/efi/efivars\" 2>/dev/null)" ] || exit 1
 
                 BOOT_HEX=$(od -An -t x1 -j 4 -N 2 /sys/firmware/efi/efivars/BootCurrent-8be4df61-93ca-11d2-aa0d-00e098032b8c | tr -d ' \n')
                 [ -z "$BOOT_HEX" ] && exit 2
@@ -43,21 +39,17 @@
                 PART_OFFSET_STR=''${VAR_HEX#*04012a00}
                 [ "$PART_OFFSET_STR" == "$VAR_HEX" ] && exit 3
 
-                RAW_UUID=''${PART_OFFSET_STR:24:32}
-                [ ''${#RAW_UUID} -n 32 ] && exit 4
+                RAW_UUID=''${PART_OFFSET_STR:40:32}
+                echo "RAW_UUID: $RAW_UUID"
+                [ ''${#RAW_UUID} -ne 32 ] && exit 4
+
                 # Little Endian fixing
-                TARGET_UUID="''${RAW_UUID:6:2}''${RAW_UUID:4:2}''${RAW_UUID:2:2}''${RAW_UUID:0:2}-''${RAW_UUID:10:2}''${RAW_UUID:8:2}-''${RAW_UUID:14:2}''${RAW_UUID:12:2}-''${RAW_UUID:16:4}-''${RAW_UUID:20:12}"
+                ESP_UUID="''${RAW_UUID:6:2}''${RAW_UUID:4:2}''${RAW_UUID:2:2}''${RAW_UUID:0:2}-''${RAW_UUID:10:2}''${RAW_UUID:8:2}-''${RAW_UUID:14:2}''${RAW_UUID:12:2}-''${RAW_UUID:16:4}-''${RAW_UUID:20:12}"
+                echo "ESP_UUID: $ESP_UUID"
 
-                DEV_PATH="/dev/disk/by-partuuid/$TARGET_UUID"
-                [ -s "$DEV_PATH" ] || exit 5
-
-                mkdir -p /sysroot/boot
-                mount "$DEV_PATH" /sysroot/boot || exit 6
+                mount PARTUUID="$ESP_UUID" /sysroot/${config.boot.loader.efi.efiSysMountPoint} || exit 5
                 exit 0
             '';
         };
     };
-# TODO: clean old code downstair
-# mount -t efivarfs efivarfs /sys/firmware/efi/efivars
-# od -An -t x1 -j 4 -N 2 /sys/firmware/efi/efivars/BootCurrent-8be4df61-93ca-11d2-aa0d-00e098032b8c | tr -d '\n'
 }
