28dd474a108b6b26801b696a58cad1db058c9650
mrChesr228 <gengenm32111111@gmail.com>
2026-06-05 15:44:24 +0300

    Comma-with-db override

--- sys/nix-index+comma.nix
+++ sys/nix-index+comma.nix
@@ -1,3 +1,6 @@
 { config, lib, pkgs, vars, self, ... }: {
+    nixpkgs.overlays = [(final: prev: {
+        comma-with-db = prev.comma-with-db.override { nix = config.nix.package; };
+    })];
     programs.nix-index-database.comma.enable = true;
 }
