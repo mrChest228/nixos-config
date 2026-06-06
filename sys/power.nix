6e40a880017773f328ff3c9c7955e5b0262154f3
mrChesr228 <gengenm32111111@gmail.com>
2026-06-06 15:09:50 +0300

    ok

--- sys/power.nix
+++ sys/power.nix
@@ -6,24 +6,24 @@
             settings = {
                 # Alternating current
                 CPU_ENERGY_PERF_POLICY_ON_AC = "perfomance";
-                CPU_SCALING_GOVERNOR_ON_AC = "schedutil"; # Smart mode
-                AMD_ENERGE_PERF_POLICY_ON_AC = "balance_power";
+                CPU_SCALING_GOVERNOR_ON_AC = "perfomance"; # amd_pstate is the main one
+                AMD_ENERGE_PERF_POLICY_ON_AC = "balance_perfomace";
                 # Battery
                 CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-                CPU_SCALING_GOVERNOR_ON_BAT = "schedutil"; # Smart mode
-                AMD_ENERGY_PERF_POLICY_ON_BAT = "balance_power"; # TODO: move specific settings to ./hardware/amd
+                CPU_SCALING_GOVERNOR_ON_BAT = "powersave"; # amd_pstate is the main one
+                AMD_ENERGY_PERF_POLICY_ON_BAT = "power"; # TODO: move specific settings to ./hardware/amd
 
                 WIFI_PWR_ON_BAT = "on";
                 SOUND_POWER_SAVE_ON_BAT = "on";
                 USB_AUTOSUSPEND = 1;
-
+                # Doesn't work on VICTUS :(
                 START_CHARGE_THRESH_BAT0 = 85;
                 STOP_CHARGE_THRESH_BAT0 = 98;
             };
         };
-        thermald.enable = true; # Temperature, fans and power control. Prevents overheating
+        thermald.enable = true; # Temperature and power control. Prevents overheating
         upower.enable = true; # Power (battery, AC) info
     };
-    hardware.sensor.lm_sensors.enable = true; # Programs can use sensors now
+    # TODO: move lm_sensors here
     # programs.mangohud.enable = true; # TODO
 }
