a614c33651117e1237e6a31457d17cfb8fd8d138
mrChesr228 <gengenm32111111@gmail.com>
2026-06-19 20:38:07 +0300

    Normal power-manager service

--- hosts/VICTUS/sys/hardware/power.nix
+++ hosts/VICTUS/sys/hardware/power.nix
@@ -1,18 +1,21 @@
 { config, lib, pkgs, vars, self, ... }: {
     # CPU power limit 65W on AC and 15W on battery and nvidia-powerd/nvidia-persisteced disabling on BAT
     environment.systemPackages = [ pkgs.ryzenadj ];
-    systemd.services.cpu-power-limits = let
+    systemd.services.power-manager = let
         ACPath = "/sys/class/power_supply/ACAD";
     in {
-        description = "Set needed CPU power limits based on AC connection";
+        description = "Manage power limits, enabled services and system settings based on AC connection";
         after = [ "multi-user.target" ];
         wantedBy = [ "multi-user.target" ];
 
+        unitConfig.StartLimitIntervalSec = 0; # Restart the service as often as I plug AC-adapter
+
         serviceConfig = {
-            # Type = "oneshot";
-            # RemainAfterExit = true;
             Type = "simple";
-            Restart = "always";
+            Restart = "on-failure";
+            RestartSec = "10s";
+            KillMode = "mixed"; # Kill the bash-script without waiting to his sleep end
+            TimeoutStopSec = "1s"; # Kill the process in 1 sec without waiting to himself killing after receiving the SIGTERM signal
         };
 
         path = with pkgs; [
@@ -22,18 +25,26 @@
 
         # TODO: slow and stapm limits 65W on AC?
         script = ''
-            while true; do
-                if [ -f ${ACPath}/online ] && [ "$(cat ${ACPath}/online)" == "1" ]; then
+            if [ -f ${ACPath}/online ] && [ "$(cat ${ACPath}/online)" == "1" ]; then
+                systemctl unmask nvidia-persistenced.service || true # Unblock nvidia-persistenced enabling
+                systemctl start nvidia-persistenced.service nvidia-powerd.service || true
+
+                while true; do
                     ryzenadj --fast-limit=65000 --slow-limit=54000 --stapm-limit=65000 --tctl-temp=97
-                    systemctl unmask nvidia-persistenced.service # Block nvidia-persistenced enabling
-                    systemctl start nvidia-persistenced.service nvidia-powerd.service
-                else
+                    sleep 3
+                done
+            else
+                systemctl stop nvidia-persistenced.service nvidia-powerd.service || true
+                systemctl mask nvidia-persistenced.service || true # Block nvidia-persistenced enabling
+
+                while true; do
                     ryzenadj --fast-limit=30000 --slow-limit=30000 --stapm-limit=30000 --tctl-temp=80
-                    systemctl stop nvidia-persistenced.service nvidia-powerd.service
-                    systemctl mask nvidia-persistenced.service
-                fi
-                sleep 3
-            done
+                    sleep 3
+                done
+            fi
         '';
     };
+    services.udev.extraRules = ''
+        SUBSYSTEM=="power_supply", ACTION=="change", RUN+="${pkgs.systemd}/bin/systemctl --no-block restart power-manager"
+    '';
 }
